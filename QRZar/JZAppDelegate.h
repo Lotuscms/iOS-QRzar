4723d2a7249adae1926832a895796d339968ffb6
Conor Brady <conorjbrady@Conors-MacBook-Pro.local>
2012-01-29 22:21:12 +0000

    Initial Commit

---
+++ QRZar/JZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JZAppDelegate.h
+//  QRZar
+//
+//  Created by Conor Brady on 29/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
